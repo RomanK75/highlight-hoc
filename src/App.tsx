69959f86a71658b4afaa6edc6ec12eec0ec58901
Mytth <Mytth75@gmail.com>
2024-07-06 14:02:17 +0700

    app new

--- src/App.tsx
+++ src/App.tsx
@@ -1,94 +1,92 @@
-import moment from 'moment';
-import React, {useState,useMemo} from 'react';
+import React, { useState } from 'react';
 
-type VideoItem = {
-    url: string,
-    date: string
-}
-type VideoListProps = {
-    list: VideoItem[]
-}
-
-type DateTimeProps = {
-    date: string
-}
-
-// HOC withPrettyDate
-const withPrettyDate = (WrappedComponent: React.FC<DateTimeProps>) => {
-  return (props: DateTimeProps) => {
-    const { date } = props;
-    const now = moment();
-    const dateTime = moment(date, 'YYYY-MM-DD HH:mm:ss');
-    const diffMinutes = now.diff(dateTime, 'minutes');
-
-    const timeStamp = useMemo(() => {
-      if (diffMinutes < 60) {
-        return `${diffMinutes} минут назад`;
-      } else if (diffMinutes < 24 * 60) {
-        return `${Math.floor(diffMinutes / 60)} часов назад`;
-      } else {
-        return `${dateTime.diff(now, 'days')} дней назад`;
-      }
-    }, [diffMinutes, dateTime, now]);
-    const prettyDate = timeStamp.replace('-','')
-    const propsWithPrettyDate = { ...props, date: prettyDate };
-
-    return <WrappedComponent {...propsWithPrettyDate} />;
-  };
+function New(props) {
+    return (
+        <div className="wrap-item wrap-item-new">
+            <span className="label">New!</span>
+            {props.children}
+        </div>
+    )
 };
 
-// Компонент DateTime
-const DateTime: React.FC<DateTimeProps> = ({ date }) => (
-  <p className="date">{date}</p>
-);
+function Popular(props) {
+    return (
+        <div className="wrap-item wrap-item-popular">
+            <span className="label">Popular!</span>
+            {props.children}
+        </div>
+    )
+};
 
-// Обернутый компонент DateTimePretty
-const DateTimePretty = withPrettyDate(DateTime);
+function Article(props) {
+    return (
+        <div className="item item-article">
+            <h3><a href="#">{props.title}</a></h3>
+            <p className="views">Прочтений: {props.views}</p>
+        </div>
+    )
+};
 
+function Video(props) {
+    return (
+        <div className="item item-video">
+            <iframe src={props.url} frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+            <p className="views">Просмотров: {props.views}</p>
+        </div>
+    )
+};
 
-// Компонент Video
-function Video(props:VideoItem) {
-  return (
-    <div className="video">
-      <iframe src={props.url} allow="autoplay; encrypted-media; fullscreen"></iframe>
-      <DateTimePretty date={props.date} />
-    </div>
-  );
-}
+function List(props) {
+    return props.list.map(item => {
+        switch (item.type) {
+            case 'video':
+                return (
+                    <Video {...item} />
+                );
 
-function VideoList(props:VideoListProps) {
-    return props.list.map(item => <Video url={item.url} date={item.date} />);
-}
+            case 'article':
+                return (
+                    <Article {...item} />
+                );
+        }
+    });
+};
 
 export default function App() {
     const [list, setList] = useState([
         {
-            url: 'https://www.youtube.com/embed/lyh2kAjcmSY?rel=0&amp;controls=0&amp;showinfo=0',
-            date: '2020-07-31 13:24:00'
+            type: 'video',
+            url: 'https://www.youtube.com/embed/rN6nlNC9WQA?rel=0&amp;controls=0&amp;showinfo=0',
+            views: 50
         },
         {
+            type: 'video',
             url: 'https://www.youtube.com/embed/dVkK36KOcqs?rel=0&amp;controls=0&amp;showinfo=0',
-            date: '2018-03-03 12:10:00'
+            views: 12
         },
         {
-            url: 'https://www.youtube.com/embed/xGRjCa49C6U?rel=0&amp;controls=0&amp;showinfo=0',
-            date: '2018-02-03 23:16:00'
+            type: 'article',
+            title: 'Невероятные события в неизвестном поселке...',
+            views: 175
         },
         {
-            url: 'https://www.youtube.com/embed/RK1K2bCg4J8?rel=0&amp;controls=0&amp;showinfo=0',
-            date: '2018-01-03 12:10:00'
+            type: 'article',
+            title: 'Секретные данные были раскрыты!',
+            views: 1532
         },
         {
+            type: 'video',
             url: 'https://www.youtube.com/embed/TKmGU77INaM?rel=0&amp;controls=0&amp;showinfo=0',
-            date: '2018-01-01 16:17:00'
+            views: 4253
         },
         {
-            url: 'https://www.youtube.com/embed/TxbE79-1OSI?rel=0&amp;controls=0&amp;showinfo=0',
-            date: '2017-12-02 05:24:00'
+            type: 'article',
+            title: 'Кот Бегемот обладает невероятной...',
+            views: 12,
         },
     ]);
 
     return (
-        <VideoList list={list} />
+        <List list={list} />
     );
 }
